72063c11e6a68ce6d64c15b64c2bb4d9b7a2cfef
Christopher Idowu <tommydprogrammer@gmail.com>
2022-07-24 12:44:00 +0100

    example folder added

---
+++ example/lib/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:paypal_flutter/paypal_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<void> getToken() async {
+    final paypal = PaypalFlutter(
+        clientId:
+            'AUv8rrc_P-EbP2E0mpb49BV7rFt3Usr-vdUZO8VGOnjRehGHBXkSzchr37SYF2GNdQFYSp72jh5QUhzG',
+        clientSecret:
+            'EMnAWe06ioGtouJs7gLYT9chK9-2jJ--7MKRXpI8FesmY_2Kp-d_7aCqff7M9moEJBvuXoBO4clKtY0v');
+    await paypal.getAccessToken();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () => getToken(),
+            child: const Text('Get Access Token'),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/paypal_flutter.dart
+++ lib/src/paypal_flutter.dart
@@ -12,13 +12,13 @@ class PaypalFlutter {
     Dio? dio,
     this.isSandbox = true,
     required this.clientId,
-    required this.secretKey,
-  }) : _dio = dio ?? Dio();
+    required this.clientSecret,
+  }) : _dio = (dio ?? Dio())..options.baseUrl = sandBoxBaseUrl;
 
   final Dio _dio;
   final bool isSandbox;
   final String clientId;
-  final String secretKey;
+  final String clientSecret;
 
   @visibleForTesting
   static String sandBoxBaseUrl = 'https://api.sandbox.paypal.com';
@@ -29,18 +29,19 @@ class PaypalFlutter {
   String get _getBaseUrl => isSandbox ? sandBoxBaseUrl : liveBaseUrl;
 
   String get basicAuth =>
-      'Basic ${base64Encode(utf8.encode('$clientId:$secretKey'))}';
+      'Basic ${base64Encode(latin1.encode('$clientId:$clientSecret'))}';
 
   Future<void> getAccessToken() async {
     try {
+      print(basicAuth);
       final response = await _dio.post(
-        '$_getBaseUrl/v1/oauth2/token',
+        '/v1/oauth2/token',
         queryParameters: {
           'grant_type': 'client_credentials',
         },
         options: Options(
           headers: {
-            'Authorization': basicAuth,
+            'Authorization': basicAuth.trim(),
           },
         ),
       );
