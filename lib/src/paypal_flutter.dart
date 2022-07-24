89ca64901fcf9b9b13bc5a91692ab50ba095cf0d
Christopher Idowu <tommydprogrammer@gmail.com>
2022-07-24 12:18:54 +0100

    basic auth added

--- lib/src/paypal_flutter.dart
+++ lib/src/paypal_flutter.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
@@ -9,10 +11,14 @@ class PaypalFlutter {
   PaypalFlutter({
     Dio? dio,
     this.isSandbox = true,
+    required this.clientId,
+    required this.secretKey,
   }) : _dio = dio ?? Dio();
 
   final Dio _dio;
   final bool isSandbox;
+  final String clientId;
+  final String secretKey;
 
   @visibleForTesting
   static String sandBoxBaseUrl = 'https://api.sandbox.paypal.com';
@@ -22,13 +28,21 @@ class PaypalFlutter {
 
   String get _getBaseUrl => isSandbox ? sandBoxBaseUrl : liveBaseUrl;
 
+  String get basicAuth =>
+      'Basic ${base64Encode(utf8.encode('$clientId:$secretKey'))}';
+
   Future<void> getAccessToken() async {
     try {
-      final response = await _dio.get(
+      final response = await _dio.post(
         '$_getBaseUrl/v1/oauth2/token',
         queryParameters: {
           'grant_type': 'client_credentials',
         },
+        options: Options(
+          headers: {
+            'Authorization': basicAuth,
+          },
+        ),
       );
       print(response);
     } catch (error) {
